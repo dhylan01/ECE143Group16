242e1acfe20ed6c748ad0388120885474461c344
Dhylan Patel <dhylan@Dhylans-MacBook-Air.local>
2024-11-09 12:45:48 -0800

    init repo w/ data

---
+++ Data/summarize_data.py
@@ -0,0 +1,15 @@
+from os import listdir
+from os.path import isfile, join
+import pandas as pd
+
+
+def get_sorted_files_from_data_dir():
+
+    onlyfiles = [f for f in listdir() if (isfile(join('', f)) and  join('', f).count('.py') <= 0)]
+    onlyfiles.sort()
+    return onlyfiles
+def merge_gun_data():
+    print(get_sorted_files_from_data_dir())
+    df = pd.concat(map(pd.read_csv,get_sorted_files_from_data_dir()),ignore_index=True)
+    df.to_csv('MergedGunData.csv',index=False)
+
